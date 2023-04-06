dd259b9359fee979a731548d0c610075421a8d43
Thalys001 <thalyssouza708@gmail.com>
2023-04-05 23:43:19 -0300

    feat: creating immutable object

--- client/js/app/models/negotiation.js
+++ client/js/app/models/negotiation.js
@@ -1,12 +1,31 @@
 class negotiation {
-  
-  constructor(data, quantity, value){
-    this.data = data;
-    this.quantity = quantity;
-    this.value = value;
+  constructor(data, quantity, value) {
+    this._data = data;
+    this._quantity = quantity;
+    this._value = value;
+    Object.freeze(this);
   }
 
-  obtemVolume() {
-    return this.quantity * this.value;
+  get volume() {
+    return this._quantity * this._value;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  get value() {
+    return this._value;
   }
 }
+
+var n1 = new negotiation(new Date(), 5, 700);
+console.log(n1.data);
+
+n1.data.setDate(11);
+
+console.log(n1.data);
